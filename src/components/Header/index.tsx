8ea7c279c478b487038aa3455ae3e972fe6779e1
chris-arghiri <cristi.arghiri13@gmail.com>
2020-12-15 14:44:21 +0200

    folder structure & setup sidebar

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Header.module.scss';
+import Sidebar from './Sidebar';
+
+const Header = () => {
+  return (
+    <div className={styles.Header}>
+      <Sidebar />
+      <h1 className={styles.Header__text}>The App is working! Don't worry</h1>;
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/containers/App/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './App.module.scss';
+import Header from '../../components/Header';
+
+const App = () => {
+  return (
+    <div className={styles.App}>
+      <Header />
+    </div>
+  );
+};
+
+export default App;
