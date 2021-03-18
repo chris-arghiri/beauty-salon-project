691654861538312b2a34ffe01ead1acac60a645e
chris-arghiri <cristi.arghiri13@gmail.com>
2021-03-18 18:19:41 +0200

    deleted slider, added swipper js library

---
+++ src/components/Swiper/index.tsx
@@ -0,0 +1,56 @@
+import { FunctionComponent } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, {
+  Navigation,
+  Pagination,
+  EffectFade,
+  Autoplay
+} from 'swiper';
+import 'swiper/swiper-bundle.css';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+
+import styles from './Swiper.module.scss';
+
+import testImage from '../../assets/images/tiny_makeup.jpg';
+import testImage2 from '../../assets/images/hair_studio.jpg';
+interface ISliderProps {}
+
+SwiperCore.use([Navigation, Pagination, EffectFade, Autoplay, ProgressEvent]);
+
+const Slider: FunctionComponent<ISliderProps> = () => {
+  return (
+    <div className={styles.Swiper}>
+      <Swiper
+        id='main'
+        navigation
+        pagination={{ clickable: true }}
+        effect='fade'
+        autoplay={{ delay: 4000, disableOnInteraction: false }}>
+        <SwiperSlide key={`slide-1`}>
+          <img
+            src={testImage}
+            alt='1'
+            style={{ listStyle: 'none', height: 'auto' }}
+          />
+        </SwiperSlide>
+        <SwiperSlide key={`slide-2`}>
+          <img
+            src={testImage2}
+            alt='2'
+            style={{ listStyle: 'none', height: 'auto' }}
+          />
+        </SwiperSlide>
+        <SwiperSlide key={`slide-3`}>
+          <img
+            src={testImage}
+            alt='3'
+            style={{ listStyle: 'none', height: 'auto' }}
+          />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Slider;
--- src/containers/Credentials/index.tsx
+++ src/containers/Credentials/index.tsx
@@ -1,21 +1,15 @@
-import React, { FunctionComponent } from 'react';
+import { FunctionComponent } from 'react';
 import styles from './Credentials.module.scss';
 
-import sliderImage1 from '../../assets/images/tiny_makeup.jpg';
-import sliderImage2 from '../../assets/images/tiny_makeup.jpg';
-import sliderImage3 from '../../assets/images/tiny_makeup.jpg';
-
 import SectionName from '../../utilities/SectionName';
-import Slider from '../../components/Slider';
+import Swiper from '../../components/Swiper';
 interface ICredentialsProps {}
 
 const Credentials: FunctionComponent<ICredentialsProps> = () => {
-  const images = [`${sliderImage1}`, `${sliderImage2}`, `${sliderImage3}`];
-
   return (
     <div className={styles.Credentials} id='credentials'>
       <SectionName name='Credentials' color='#fff' top='5vh' left='7vw' />
-      <Slider slides={images} />
+      <Swiper />
     </div>
   );
 };
