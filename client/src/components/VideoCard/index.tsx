27fcc7f7194b831577d2f595be217957ac5d38e6
chris-arghiri <cristi.arghiri13@gmail.com>
2021-03-21 20:48:41 +0200

    added shop section p.s. what a 15 min writting code when you/re tired can look like

--- client/src/components/VideoCard/index.tsx
+++ client/src/components/VideoCard/index.tsx
@@ -2,13 +2,16 @@ import React, { FunctionComponent } from 'react';
 import styles from './VideoCard.module.scss';
 
 interface IVideoCardProps {
-  url: string;
+  url: string | undefined;
+  autoplay: boolean;
 }
 
-const VideoCard: FunctionComponent<IVideoCardProps> = ({ url }) => (
+const VideoCard: FunctionComponent<IVideoCardProps> = ({ url, autoplay }) => (
   <div className={styles.VideoCard}>
     <video
       src={url}
+      autoPlay={autoplay}
+      muted
       onMouseOver={(event) => (event.target as HTMLMediaElement).play()}
       onMouseOut={(event) => (event.target as HTMLMediaElement).pause()}
     />
--- client/src/containers/Videos/index.tsx
+++ client/src/containers/Videos/index.tsx
@@ -1,23 +1,52 @@
-import React, { FunctionComponent } from 'react';
+import React, {
+  FunctionComponent,
+  useCallback,
+  useEffect,
+  useState
+} from 'react';
 import styles from './Videos.module.scss';
+
+import axios from 'axios';
 
 import SectionName from '../../utilities/SectionName';
 import VideoCard from '../../components/VideoCard';
 
-import productionVideo from '../../assets/videos/production.mp4';
-import image1 from '../../assets/images/blonde-hair.jpeg';
-import image2 from '../../assets/images/blonde-hair.jpg';
-
 interface IVideosProps {}
 
 const Videos: FunctionComponent<IVideosProps> = () => {
+  const [url, setURL] = useState<string | undefined>('');
+
+  const fetchAPI = useCallback(async () => {
+    await axios
+      .get('https://api.pexels.com/videos/videos/3996898', {
+        headers: {
+          Authorization:
+            '563492ad6f91700001000001c7556e8ee0f24cb6a0d7a0291a166a7b',
+          Accept: 'application/json'
+        }
+      })
+      .then((result) => {
+        const data = result.data;
+        const videoFiles = data.video_files;
+        const link = videoFiles[0].link;
+        setURL(link);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetchAPI();
+  }, [fetchAPI]);
+
   return (
     <div className={styles.Videos} id='videos'>
       <SectionName name='Videos' color='#fff' top='3vh' left='7vw' />
       <div className={styles.Videos__cards}>
-        <VideoCard url={productionVideo} />
-        <VideoCard url={image1} />
-        <VideoCard url={image2} />
+        <VideoCard url={url} autoplay={true} />
+        <VideoCard url={url} autoplay={false} />
+        <VideoCard url={url} autoplay={false} />
       </div>
     </div>
   );
